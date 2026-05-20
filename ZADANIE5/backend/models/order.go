76873047e8015f34fc58a832278fe68818efa287
lukasz1_malec <lmalec493@gmail.com>
2026-05-20 18:38:26 +0200

    Dodanie walidacji dla modeli

--- ZADANIE5/backend/models/order.go
+++ ZADANIE5/backend/models/order.go
@@ -1,5 +1,9 @@
 package models
-import "time"
+import (
+	"errors"
+	"time"
+)
+
 
 type Customer struct {
 	Name    string `json:"name"`
@@ -21,4 +25,49 @@ type Order struct {
 	Customer  Customer    `json:"customer"`
 	Status    string      `json:"status"`
 	CreatedAt time.Time   `json:"created_at"`
+}
+
+
+// walidacja danych
+
+func (i OrderItem) Validate() error {
+	if i.Name == "" {
+		return errors.New("nazwa pozycji nie może być pusta")
+	}
+	if i.Price <= 0 {
+		return errors.New("cena pozycji musi być większa od zera")
+	}
+	if i.Quantity <= 0 {
+		return errors.New("ilość musi być większa od zera")
+	}
+	if i.ProductID <= 0 {
+		return errors.New("product_id musi być większy od zera")
+	}
+	return nil
+}
+
+func (c Customer) Validate() error {
+	if c.Name == "" {
+		return errors.New("nazwa klienta nie może być pusta")
+	}
+	if c.Email == "" {
+		return errors.New("email klienta nie może być pusty")
+	}
+	if c.Address == "" {
+		return errors.New("adres klienta nie może być pusty")
+	}
+	return nil
+}
+
+func (o Order) Validate() error {
+	if o.Total <= 0 {
+		return errors.New("suma zamówienia musi być większa od zera")
+	}
+	if len(o.Items) == 0 {
+		return errors.New("zamówienie musi mieć co najmniej jedną pozycję")
+	}
+	if err := o.Customer.Validate(); err != nil {
+		return err
+	}
+	return nil
 }
